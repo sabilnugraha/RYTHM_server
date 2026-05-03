f5400f44723c6ca5703eba4f9069a987e0c8ea50
Sabilly Nugraha <101251168+sabilnugraha@users.noreply.github.com>
2026-05-03 21:36:14 +0700

    Register platform user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import healthRoutes from './health.routes.js';
+import platformUserRoutes from '../modules/platform-users/platform-user.routes.js';
 
 const router = Router();
 
 router.use('/health', healthRoutes);
+router.use('/platform-users', platformUserRoutes);
 
 export default router;
